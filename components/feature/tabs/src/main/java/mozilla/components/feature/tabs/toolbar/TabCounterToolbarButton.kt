81c1fd9fbdff6baa22c2e7eec1d9bf74ae5ab622
Elise Richards <erichards@mozilla.com>
2020-10-29 14:51:29 -0500

    Replace ac tab counter toolbar button with fenix impl

--- components/feature/tabs/src/main/java/mozilla/components/feature/tabs/toolbar/TabCounterToolbarButton.kt
+++ components/feature/tabs/src/main/java/mozilla/components/feature/tabs/toolbar/TabCounterToolbarButton.kt
@@ -4,67 +4,81 @@
 
 package mozilla.components.feature.tabs.toolbar
 
-import android.view.HapticFeedbackConstants
 import android.view.View
 import android.view.ViewGroup
-import mozilla.components.browser.session.Session
-import mozilla.components.browser.session.SessionManager
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import mozilla.components.browser.state.selector.getNormalOrPrivateTabs
+import mozilla.components.browser.state.store.BrowserStore
 import mozilla.components.concept.toolbar.Toolbar
-import mozilla.components.feature.tabs.R
+import mozilla.components.lib.state.ext.flowScoped
 import mozilla.components.support.ktx.android.content.res.resolveAttribute
+import mozilla.components.support.ktx.kotlinx.coroutines.flow.ifChanged
 import mozilla.components.ui.tabcounter.TabCounter
 import java.lang.ref.WeakReference
 
 /**
  * A [Toolbar.Action] implementation that shows a [TabCounter].
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 class TabCounterToolbarButton(
-    private val sessionManager: SessionManager,
-    private val showTabs: () -> Unit
+    private val lifecycleOwner: LifecycleOwner,
+    private val isPrivate: Boolean,
+//    private val onItemTapped: (TabCounterMenu.Item) -> Unit = {},
+    private val showTabs: () -> Unit,
+    private val store: BrowserStore
 ) : Toolbar.Action {
+
     private var reference: WeakReference<TabCounter> = WeakReference<TabCounter>(null)
 
     override fun createView(parent: ViewGroup): View {
-        sessionManager.register(sessionManagerObserver, view = parent)
+//        sessionManager.register(sessionManagerObserver, view = parent)
+        
+        // these should be replaced in the fenix impl
+//        val metrics = parent.context.components.analytics.metrics
+//        val settings = parent.context.components.settings
+
+        store.flowScoped(lifecycleOwner) { flow ->
+            flow.map { state -> state.getNormalOrPrivateTabs(isPrivate).size }
+                .ifChanged()
+                .collect { tabs -> updateCount(tabs) }
+        }
+
+//        val menu = TabCounterMenu(parent.context, metrics, onItemTapped)
+//        menu.updateMenu(settings.toolbarPosition)
 
         val view = TabCounter(parent.context).apply {
             reference = WeakReference(this)
-            setCount(sessionManager.sessions.size)
             setOnClickListener {
-                it.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
                 showTabs.invoke()
             }
-            contentDescription = parent.context.getString(R.string.mozac_feature_tabs_toolbar_tabs_button)
+
+//            setOnLongClickListener {
+//                menu.menuController.show(anchor = it)
+//                true
+//            }
+
+            addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+                override fun onViewAttachedToWindow(v: View?) {
+                    setCount(store.state.getNormalOrPrivateTabs(isPrivate).size)
+                }
+
+                override fun onViewDetachedFromWindow(v: View?) { /* no-op */ }
+            })
         }
 
         // Set selectableItemBackgroundBorderless
-        val selectableItemBackgroundBorderless =
-            parent.context.theme.resolveAttribute(android.R.attr.selectableItemBackgroundBorderless)
-        view.setBackgroundResource(selectableItemBackgroundBorderless)
+        view.setBackgroundResource(parent.context.theme.resolveAttribute(
+            android.R.attr.selectableItemBackgroundBorderless
+        ))
         return view
     }
 
     override fun bind(view: View) = Unit
 
-    private fun updateCount() {
-        reference.get()?.setCountWithAnimation(sessionManager.sessions.size)
-    }
-
-    private val sessionManagerObserver = object : SessionManager.Observer {
-        override fun onSessionAdded(session: Session) {
-            updateCount()
-        }
-
-        override fun onSessionRemoved(session: Session) {
-            updateCount()
-        }
-
-        override fun onSessionsRestored() {
-            updateCount()
-        }
-
-        override fun onAllSessionsRemoved() {
-            updateCount()
-        }
+    private fun updateCount(count: Int) {
+        reference.get()?.setCountWithAnimation(count)
     }
 }
